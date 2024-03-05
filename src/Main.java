5460e63534005cbe20fe78b2ef2ae4a588c2fc2f
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-05 15:09:30 +0530

    Transform each element to double

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
         Stream<Integer> stream = Stream.of(1,2,3,4,5);
         //Iterating through stream
         stream.forEach(n -> System.out.println(n));
+
+        //Transform each element to doublegit
+        Stream<Integer> stream1 = Stream.of(1,2,3,4,5);
+        Stream<Integer> doubleStream = stream1.map(n -> n * 2);
+        doubleStream.forEach(n-> System.out.println(n));
     }
 }
