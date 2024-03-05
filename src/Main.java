a68587279eb43bb3c7771991e9b41a073106a3f1
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-05 15:37:01 +0530

    filter the even numbers from the number Stream

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         //creating stream
-        Stream<Integer> stream = Stream.of(1,2,3,4,5);
+        Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9);
         //Iterating through stream
-        stream.forEach(n -> System.out.println(n));
+//        stream.forEach(n -> System.out.println(n));
 
         //Transform each element to doublegit
         Stream<Integer> stream1 = Stream.of(1,2,3,4,5);
@@ -17,5 +17,9 @@ public class Main {
         //to store value into a new List
         List<Integer> doubleValue = stream1.map(n-> n*2).collect(Collectors.toList());
         System.out.println(doubleValue);
+
+        //filter the even numbers from the number Stream
+        Stream<Integer> evenStream = stream.filter(n -> n % 2 == 0);
+        evenStream.forEach(n-> System.out.println(n));
     }
 }
